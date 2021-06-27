ce4e3ab1d8816207f74a011300f5fe7e89b50dae
Kanchan Jeswani <kanchanjeswani2000@gmail.com>
2021-06-27 18:12:05 +0530

    Addded solution for the problem in cpp

---
+++ Longest Common Substring.cpp
@@ -0,0 +1,28 @@
+    int longestCommonSubstr (string str1, string str2, int n, int m)
+    {
+        int dp[n + 1][m + 1];
+        memset(dp, -1, sizeof(dp));
+        int ans = 0;
+        for(int i = 0; i <= n; i++)
+        {
+            for(int j = 0; j <= m;j++)
+            {
+                 if(i == 0 || j == 0)
+                 {
+                     dp[i][j] = 0;
+                 }
+                 else if(str1[i - 1] == str2[j - 1])
+                 {
+                     dp[i][j] = 1 + dp[i - 1][j - 1];
+                     ans = max(ans, dp[i][j]);
+                 }
+                 else
+                 {
+                     dp[i][j] = 0;
+                 }
+            }
+        }
+        return ans;
+    }
+    // tc o(nm)
+    // sc o(nm)
