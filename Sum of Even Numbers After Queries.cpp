53d4aa945263400c1c3f370ad3b078b9755bbf5b
Kanchan Jeswani <kanchanjeswani2000@gmail.com>
2021-07-14 01:31:34 +0530

    added solution for the problem in cpp

---
+++ Sum of Even Numbers After Queries.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> sumEvenAfterQueries(vector<int>& nums, vector<vector<int>>& queries) 
+    {
+        int sum = 0;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(nums[i] % 2 == 0)
+            {
+                sum += nums[i];   
+            }
+        }
+        vector<int>ans;
+        for(int i = 0; i < queries.size(); i++)
+        {
+            int value = queries[i][0];
+            int index = queries[i][1];
+            if(nums[index] % 2 == 0)
+            {
+                sum -= nums[index];
+            }
+            nums[index] += value;
+            if(nums[index] % 2 == 0)
+            {
+                sum += nums[index];
+            }
+            ans.push_back(sum);
+        }
+        return ans;
+    }
+};
+// tc o(n)
+// sc o(1)
